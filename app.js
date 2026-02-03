5a194fe272c0511f775d96d940801bbe7c188a2b
rafaelm-perezc <rafael.perez@sedhuila.edu.co>
2026-02-03 12:35:20 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    SistemaProyectos.exe
    	modified:   app.js El servidor se mantiene activo por 30 minutos sin actividad.
    	deleted:    src/db/proyectos_huila.db

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const multer = require('multer');
 const projectController = require('./src/controllers/projectController');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
+const DEFAULT_PORT = isProduction ? 8080 : 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
 
 // 1. Middlewares
 app.use(express.json());
